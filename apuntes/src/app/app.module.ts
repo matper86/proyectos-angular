400cab972d9fbe22d6e91afb2b2929c66a06d06f
mepm86 <matper86-github@yahoo.com>
2023-06-02 16:10:29 -0300

    16:10 2-6-23

--- apuntes/src/app/app.module.ts
+++ apuntes/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MatiasComponent } from './matias/matias.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MatiasComponent
   ],
   imports: [
     BrowserModule
