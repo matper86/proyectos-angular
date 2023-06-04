8908f857aefcd052fc175c7b9c098f7f40e32306
mepm86 <matper86-github@yahoo.com>
2023-06-04 19:21:18 -0300

    21

---
+++ apuntes/src/app/pages/markdown/markdown.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-markdown',
+  templateUrl: './markdown.component.html',
+  styleUrls: ['./markdown.component.css']
+})
+export class MarkdownComponent {
+
+}
