b8d7bfb6c4f350666c50d0f53a0d51d1803d5f69
mepm86 <matper86-github@yahoo.com>
2023-06-26 20:26:45 -0300

    upg4

--- porfolio/src/app/header/header.module.ts
+++ porfolio/src/app/header/header.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TituloComponent } from './titulo/titulo.component';
+import { BannerComponent } from './banner/banner.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TituloComponent,
+    BannerComponent
+  ],
   imports: [
     CommonModule
   ]
