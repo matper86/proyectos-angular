01c7dcfca2beadaddf364d57a9d421b024b46bea
mepm86 <matper86-github@yahoo.com>
2023-06-26 17:43:07 -0300

    upg01

---
+++ curso-angular/src/app/models/person.ts
@@ -0,0 +1,11 @@
+export class Person {
+  nombre: string;
+  apellido: string;
+  edad: number;
+
+  constructor(edad:number, nombre:string, apellido:string) {
+  this.edad=edad;
+  this.nombre=nombre;
+  this.apellido=apellido;
+  }
+}
---
+++ curso-angular/src/app/person-administration/person-administration.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PersonComponentComponent } from './person-component/person-component.component';
+
+
+
+@NgModule({
+  declarations: [
+    PersonComponentComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PersonAdministrationModule { }
